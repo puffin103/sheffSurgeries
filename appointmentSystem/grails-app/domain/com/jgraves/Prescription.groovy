b22b4c9ca957bf63a8b54e40fb4254f97bd64cbc
puffin103 <b7023778@my.shu.ac.uk>
2019-03-11 14:40:50 +0000

    Added data types and contraints to the Prescription class

--- appointmentSystem/grails-app/domain/com/jgraves/Prescription.groovy
+++ appointmentSystem/grails-app/domain/com/jgraves/Prescription.groovy
@@ -2,6 +2,20 @@ package com.jgraves
 
 class Prescription {
 
+String pharmacyName
+int prescripNumber
+String medicine
+Double totalCost
+Date dateIssued
+Boolean patientPaying
+
     static constraints = {
+
+pharmacyName blank:false
+prescripNumber blank:false, unique:true
+medicine blank:false
+totalCost blank:false, scale:3
+dateIssued blank:false
+patientPaying blank:false
     }
 }
