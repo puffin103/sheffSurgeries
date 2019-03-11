38c288f68493e6c4e5853f6d9184d84f05064852
puffin103 <b7023778@my.shu.ac.uk>
2019-03-11 14:10:12 +0000

    Added data types and contraints to the Nurse class

--- appointmentSystem/grails-app/domain/com/jgraves/Nurse.groovy
+++ appointmentSystem/grails-app/domain/com/jgraves/Nurse.groovy
@@ -2,6 +2,21 @@ package com.jgraves
 
 class Nurse {
 
+String nurseName
+String nurseEmail
+String qualifications
+Double nurseOffice
+int nursePhone
+
+
     static constraints = {
+
+nurseName blank:false
+nurseEmail email:true, blank:false
+qualifications blank:false
+nurseOffice blank:false
+nursePhone blank:false, maxSize:11
+
+
     }
 }
