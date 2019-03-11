5e601d51895fc5b0173969763233e05f8500f7a1
puffin103 <b7023778@my.shu.ac.uk>
2019-03-11 15:03:51 +0000

    Added data types and constraints to the Receptionist class

--- appointmentSystem/grails-app/domain/com/jgraves/Receptionist.groovy
+++ appointmentSystem/grails-app/domain/com/jgraves/Receptionist.groovy
@@ -2,6 +2,19 @@ package com.jgraves
 
 class Receptionist {
 
+String recepName
+String recepEmail
+String recepUsername
+String recepPassword 
+int recepPhone
+
     static constraints = {
+
+recepName blank:false
+recepEmail email:true, blank:false
+recepUsername unique:true, blank:false
+recepPassword blank:false, min:6, max:16
+recepPhone blank:false, min:11, max:11
+
     }
 }
